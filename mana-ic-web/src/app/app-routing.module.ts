ac1387c40c41424f9a104243678d66f58a9fbd80
Anawat <anawat@perfenterprise.com>
2020-03-03 15:12:01 +0700

    add list page and change routing to hash type
    Signed-off-by: Anawat <anawat@perfenterprise.com>

--- mana-ic-web/src/app/app-routing.module.ts
+++ mana-ic-web/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  {
+    path: 'list',
+    loadChildren: () => import('./list/list.module').then( m => m.ListPageModule)
+  },
+
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { useHash: true })
   ],
   exports: [RouterModule]
 })
