d848f26851b3228b51ea82f56d8ccaa1ac31e8b8
Anawat <anawat@perfenterprise.com>
2020-03-03 15:11:26 +0700

    add web and db config
    Signed-off-by: Anawat <anawat@perfenterprise.com>

---
+++ ManaIC/Models/WebConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManaIC.Models
+{
+    public class WebConfig
+    {
+        public string PromomeServiceId { get; set; }
+        public string PromomeMerchantId { get; set; }
+    }
+}
--- ManaIC/Startup.cs
+++ ManaIC/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ManaIC.Models;
 
 namespace ManaIC
 {
@@ -33,6 +34,11 @@ namespace ManaIC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var webConfig = Configuration.GetSection(nameof(WebConfig)).Get<WebConfig>();
+            var dbConfig = Configuration.GetSection(nameof(DbConfig)).Get<DbConfig>();
+            services.AddTransient(it => dbConfig);
+            services.AddTransient(it => webConfig);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
