2d26066651587f899863662926f7d5733f6a2043
perawatt <pekung4433@perfenterprise.com>
2020-03-04 15:45:37 +0700

    binding register (incomplete)

--- mana-ic-web/src/app/register/register.page.ts
+++ mana-ic-web/src/app/register/register.page.ts
@@ -9,14 +9,26 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 export class RegisterPage implements OnInit {
 
   public fg: FormGroup;
+  statusType: string;
+  Student: string = "Student";
+  Scholar: string = "Scholar";
+  Guest: string = "Guest";
+  universityType: string;
+  kku: string = "kku";
+  other: string = "other";
 
   constructor(private fb: FormBuilder) {
     this.fg = this.fb.group({
       'nameTH': [null, Validators.required],
+      'status': [this.statusType, Validators.required],
+      'registerPoster': Boolean,
+      'registerMovie': Boolean,
+      'registerCosplay': Boolean,
+      'registerROV': Boolean,
     });
   }
 
-  async ngOnInit() {  }
+  async ngOnInit() { }
 
   submit() {
     console.log(this.fg.value)
