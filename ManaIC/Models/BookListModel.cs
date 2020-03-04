1a6653818b180aba9c24b8f89555c8529f131aa6
Anawat <anawat@perfenterprise.com>
2020-03-04 11:59:06 +0700

    add poster register to model
    Signed-off-by: Anawat <anawat@perfenterprise.com>

--- ManaIC/Models/BookListModel.cs
+++ ManaIC/Models/BookListModel.cs
@@ -10,20 +10,23 @@ namespace ManaIC.Models
         public string Id { get; set; }
         public string NameTH { get; set; }
         public string NameEN { get; set; }
-        public string MyProperty { get; set; }
+        public string Status { get; set; }
         public string Affiliation { get; set; }
         public string KKUStudentID { get; set; }
-        public string RegisterROV { get; set; }
+        public bool RegisterROV { get; set; }
         public string ROVTeam { get; set; }
-        public string RegisterCosplay { get; set; }
+        public bool RegisterCosplay { get; set; }
+        public string Nickname { get; set; }
         public string RefCharacter { get; set; }
-        public string RegisterMovie { get; set; }
+        public bool RegisterMovie { get; set; }
         public string MovieTeam { get; set; }
+        public bool RegisterPoster { get; set; }
+        public string PosterTeam { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime? DeleteDate { get; set; }
         public DateTime? FirstDate { get; set; }
         public DateTime? SecondDate { get; set; }
-        public DateTime? thidDate { get; set; }
+        public DateTime? ThirdDate { get; set; }
     }
 
     public class Status
